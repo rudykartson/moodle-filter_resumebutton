d92339d272b19f51968819ae0d9180fc96842c7d
Rudraksh Batra <38044398+rudykartson@users.noreply.github.com>
2025-08-22 11:23:03 +0530

    Delete resumebutton/uninstall.php
    uninstall not required

--- resumebutton/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-function xmldb_filter_resumebutton_uninstall() {
-    // Clean up if needed
-    unset_config('usageinstructions', 'filter_resumebutton');
-    return true;
-}
