b24478f5a476d5f1bc4986b3557a0fca4d2efa38
Rudraksh Batra <38044398+rudykartson@users.noreply.github.com>
2025-08-27 11:21:02 +0530

    Update version.php

--- resumebutton/version.php
+++ resumebutton/version.php
@@ -7,7 +7,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_resumebutton';
-$plugin->version = 2025072302;
+$plugin->version = 2025072303;
 $plugin->requires = 2022041900; // Moodle 4.0+
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.1';
